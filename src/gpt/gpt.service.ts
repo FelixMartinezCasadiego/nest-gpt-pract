227b31908755ababaeeea8ff4843b3a6d97348ab
Felix Martinez Casadiego <felix.martinez@simonelectric.com>
2025-07-23 22:30:55 +0200

    feat: integrate OpenAI API for orthography checks and update dependencies

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -5,11 +5,18 @@ import { orthographyUseCase } from './use-cases';
 
 /* Dtos */
 import { OrthographyDto } from './dtos';
+import OpenAI from 'openai';
 
 @Injectable()
 export class GptService {
+  private openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
   // use cases
   async orthographyCheck(orthographyDto: OrthographyDto) {
-    return await orthographyUseCase({ prompt: orthographyDto.prompt });
+    return await orthographyUseCase(this.openai, {
+      prompt: orthographyDto.prompt,
+    });
   }
 }
--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -1,9 +1,17 @@
+import OpenAI from 'openai';
+
 interface Options {
   prompt: string;
 }
 
-export const orthographyUseCase = async (options: Options) => {
-  const { prompt } = options;
+export const orthographyUseCase = async (openai: OpenAI, options: Options) => {
+  const completions = await openai.chat.completions.create({
+    messages: [
+      { role: 'system', content: 'You are a helpful assistant.' },
+      { role: 'user', content: options.prompt },
+    ],
+    model: 'gpt-4.1-nano',
+  });
 
-  return { prompt: prompt, apikey: process.env.OPENAI_API_KEY };
+  return completions.choices[0];
 };
