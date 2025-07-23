48935bb1a37c280f89f566290d4f92a143228409
Felix Martinez Casadiego <felix.martinez@simonelectric.com>
2025-07-23 21:10:12 +0200

    feat: add DTOs for orthography check and validation pipes

--- src/gpt/gpt.controller.ts
+++ src/gpt/gpt.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+
+/* Services */
 import { GptService } from './gpt.service';
+
+/* Dtos */
+import { OrthographyDto } from './dtos';
 
 @Controller('gpt')
 export class GptController {
   constructor(private readonly gptService: GptService) {}
 
   @Post('orthography-check')
-  orthographyCheck() {
-    return this.gptService.orthographyCheck();
+  orthographyCheck(@Body() orthographyDto: OrthographyDto) {
+    return orthographyDto;
+    // return this.gptService.orthographyCheck();
   }
 }
