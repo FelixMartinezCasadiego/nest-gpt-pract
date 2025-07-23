29fb2d4a522c6999ad730c3141e0ec094b8e8cd8
Felix Martinez Casadiego <felix.martinez@simonelectric.com>
2025-07-23 22:57:09 +0200

    feat: update orthographyUseCase file with new content in system role

--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -7,11 +7,39 @@ interface Options {
 export const orthographyUseCase = async (openai: OpenAI, options: Options) => {
   const completions = await openai.chat.completions.create({
     messages: [
-      { role: 'system', content: 'You are a helpful assistant.' },
+      {
+        role: 'system',
+        content: `
+        Te serán proveídos textos en varios idiomas con posibles errores ortográficos y grámaticales.
+        Las palabras deben existir en el idioma del texto, y no deben ser inventadas.
+        Tu tarea es corregirlos y devolver el texto corregido en formato JSON,
+        también debes de dar un porcentaje de acierto por el usuario,
+
+        Si no hay errores, debes de retornar un mensaje de felicitaciones.
+
+        ejemplo de salida:
+        {
+          userScore:number,
+          errors:[], // ['error -> solución]
+          message: string // Usa emojis y texto para felicitar al usuario si no hay errores
+        }
+        `,
+      },
       { role: 'user', content: options.prompt },
     ],
     model: 'gpt-4.1-nano',
+    max_tokens: 150,
   });
 
-  return completions.choices[0];
+  console.log('Completions:', {
+    prompt_tokens: completions.usage?.prompt_tokens,
+    completion_tokens: completions.usage?.completion_tokens,
+    total_tokens: completions.usage?.total_tokens,
+  });
+
+  const jsonResponse = JSON.parse(
+    completions.choices[0].message.content ?? '{}',
+  );
+
+  return jsonResponse;
 };
