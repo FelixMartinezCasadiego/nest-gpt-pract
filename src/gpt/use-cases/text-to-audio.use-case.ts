25debf72c0082b9b54a9999263d519ec24878d5a
Felix Martinez Casadiego <felix.martinez@simonelectric.com>
2025-08-02 22:06:36 +0200

    feat: create textToAudioUseCase.tsx file and integrate with openai

--- src/gpt/use-cases/text-to-audio.use-case.ts
+++ src/gpt/use-cases/text-to-audio.use-case.ts
@@ -1,4 +1,6 @@
 import OpenAI from 'openai';
+import * as path from 'path';
+import * as fs from 'fs';
 
 interface Options {
   prompt: string;
@@ -8,9 +10,32 @@ interface Options {
 export const textToAudioUseCase = async (openai: OpenAI, options: Options) => {
   const { voice, prompt } = options;
 
-  const voices = { nova: 'nova', alloy: 'alloy' };
+  const voices = {
+    nova: 'nova',
+    alloy: 'alloy',
+    echo: 'echo',
+    fable: 'fable',
+    onyx: 'onyx',
+    shimmer: 'shimmer',
+  };
 
   const selectedVoice = voices[voice ?? 'nova'] ?? 'nova';
 
-  return { prompt, selectedVoice };
+  const folderPath = path.resolve(__dirname, '../../../generated/audios');
+  const speechFile = path.resolve(`${folderPath}/${new Date().getTime()}.mp3`);
+
+  fs.mkdirSync(folderPath, { recursive: true });
+
+  const mp3 = await openai.audio.speech.create({
+    model: 'gpt-4o-mini-tts',
+    voice: selectedVoice,
+    input: prompt,
+    response_format: 'mp3',
+  });
+
+  const buffer = Buffer.from(await mp3.arrayBuffer());
+
+  fs.writeFileSync(speechFile, buffer);
+
+  return speechFile;
 };
